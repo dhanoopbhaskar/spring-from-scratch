2ac71f6cdc2036454675c57dbc74f083304aba92
dhanoopbhaskar <dhanoopbhaskar@gmail.com>
2022-05-26 14:15:17 +0530

    Error: Web application could not be started as there was no org.springframework.boot.web.servlet.server.ServletWebServerFactory bean defined in the context.

---
+++ src/main/java/in/theinsanetechie/demo/SpringFromScratchApplication.java
@@ -0,0 +1,13 @@
+package in.theinsanetechie.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringFromScratchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFromScratchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/theinsanetechie/demo/SpringFromScratchApplicationTests.java
@@ -0,0 +1,13 @@
+package in.theinsanetechie.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFromScratchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
