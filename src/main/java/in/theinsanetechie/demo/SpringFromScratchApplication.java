d8abc459af5bcae515ada26dd57b2431910c186d
dhanoopbhaskar <dhanoopbhaskar@gmail.com>
2022-05-26 14:29:04 +0530

    Added TomcatServletWebServerFactory bean

--- src/main/java/in/theinsanetechie/demo/SpringFromScratchApplication.java
+++ src/main/java/in/theinsanetechie/demo/SpringFromScratchApplication.java
@@ -1,13 +1,21 @@
 package in.theinsanetechie.demo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SpringFromScratchApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringFromScratchApplication.class, args);
-	}
+    @Bean
+    public TomcatServletWebServerFactory servletContainer() {
+        return new TomcatServletWebServerFactory();
+    }
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringFromScratchApplication.class, args);
+    }
 
 }
